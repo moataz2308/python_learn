179a05b5474ea497b50c6dbe11ae1c8d6a4b871c
Moataz <moataz.odense@gmail.com>
2023-03-04 21:27:13 +0100

    Triangel with a circle

---
+++ seschion6.py
@@ -0,0 +1,22 @@
+import turtle
+screen=turtle.getscreen()
+x=turtle.Turtle()
+x.color('purple')
+x.begin_fill()
+x.forward(200)
+x.right(120)
+x.forward(200)
+x.right(120)
+x.forward(200)
+x.end_fill()
+x.circle(50)
+
+
+
+
+
+
+
+
+
+screen.exitonclick()

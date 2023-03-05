eb449ba2dbd6a4a1819265346b3623397a440876
Moataz <moataz.odense@gmail.com>
2023-03-05 11:44:18 +0100

    add circles

---
+++ circle.py
@@ -0,0 +1,15 @@
+import turtle
+screen=turtle.getscreen()
+x=turtle.Turtle()
+x.color('blue')
+x.begin_fill()
+x.circle(50)
+x.forward(60)
+x.color('orange')
+x.circle(50)
+x.forward(60)
+x.color('black')
+x.circle(50)
+x.forward(60)
+x.color('red')
+

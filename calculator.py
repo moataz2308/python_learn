25cecc049cda8042b0f5f9b7ec5043a809d6b4a2
Moataz <moataz.odense@gmail.com>
2023-04-01 18:19:22 +0200

    calcutar ein paar sachen hinyugefügt

--- calculator.py
+++ calculator.py
@@ -5,14 +5,16 @@ window.geometry('400x200')
 num1=StringVar()
 num2=StringVar()
 resultVar=StringVar()
-u_first_num=Label(window,bg="blue",fg="white",font="arial",width=15,text='enter first number ',borderwidth=2, relief="groove")
+u_first_num=Entry(window,bg="blue",fg="white",font="arial",width=15,text='1 ',borderwidth=2, relief="groove")
 u_first_num.pack()
-u_first_num=Entry(window,bg="green",width=20,font="arial")
-u_first_num.pack()
-u_second_num=Label(window,bg="blue",fg="white",font="arial",width=15,text='enter second number ',borderwidth=2, relief="groove")
+u_second_num=Entry(window,bg="blue",fg="white",font="arial",width=15,text='2 ',borderwidth=2, relief="groove")
 u_second_num.pack()
-u_second_num=Entry(window,bg="green",width=20,font="arial")
-u_second_num.pack()
+u_third_num=Entry(window,bg="blue",fg="white",font="arial",width=15,text='3 ',borderwidth=2, relief="groove")
+u_third_num.pack()
+u_fourth_num=Entry(window,bg="blue",fg="white",font="arial",width=15,text='4',borderwidth=2, relief="groove")
+u_fourth_num.pack()
+
+
 def printsum():
     uFirst=u_first_num.get()
     uSecond=u_second_num.get()
@@ -20,7 +22,7 @@ def printsum():
     num1=int(uFirst)
     result=num1+num2
     resultVar.set(result)
-btn=Button(window,bg="red",width=20,text="enter the addition", font= "arial",command=printsum)
+btn=Button(window,bg="red",width=1,text="+", font= "arial",command=printsum)
 btn.pack()
 def printsub():
     uFirst=u_first_num.get()
@@ -29,7 +31,7 @@ def printsub():
     num1=int(uFirst)
     result=num1-num2
     resultVar.set(result)
-btn=Button(window,bg="red",width=20,text="enter the subtraction", font= "arial",command=printsub)
+btn=Button(window,bg="red",width=1,text="-", font= "arial",command=printsub)
 btn.pack()
 def printmul():
     uFirst=u_first_num.get()
@@ -38,7 +40,7 @@ def printmul():
     num1=int(uFirst)
     result=num1*num2
     resultVar.set(result)
-btn=Button(window,bg="red",width=20,text="enter the multiplicitation", font= "arial",command=printmul)    
+btn=Button(window,bg="red",width=1,text="*", font= "arial",command=printmul)    
 btn.pack()
 def printdiv():
     uFirst=u_first_num.get()
@@ -47,7 +49,7 @@ def printdiv():
     num1=int(uFirst)
     result=num1/num2
     resultVar.set(result)
-btn=Button(window,bg="red",width=20,text="enter the divide", font= "arial",command=printdiv)    
+btn=Button(window,bg="red",width=1,text="/", font= "arial",command=printdiv)    
 btn.pack()   
 labeleage=Label(window,bg="grey",fg="yellow",font="arial",width="30", textvariable= resultVar)
 labeleage.pack()

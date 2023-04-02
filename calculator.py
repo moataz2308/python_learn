f4ec351b666808ea9ec712fa82d6a36f66463642
Moataz <moataz.odense@gmail.com>
2023-04-02 09:54:07 +0200

    grad besseesr machen

--- calculator.py
+++ calculator.py
@@ -6,51 +6,162 @@ num1=StringVar()
 num2=StringVar()
 resultVar=StringVar()
 u_first_num=Entry(window,bg="blue",fg="white",font="arial",width=15,text='1 ',borderwidth=2, relief="groove")
-u_first_num.pack()
-u_second_num=Entry(window,bg="blue",fg="white",font="arial",width=15,text='2 ',borderwidth=2, relief="groove")
-u_second_num.pack()
-u_third_num=Entry(window,bg="blue",fg="white",font="arial",width=15,text='3 ',borderwidth=2, relief="groove")
-u_third_num.pack()
-u_fourth_num=Entry(window,bg="blue",fg="white",font="arial",width=15,text='4',borderwidth=2, relief="groove")
-u_fourth_num.pack()
+u_first_num.pack() 
+
+btn=Button(window,bg="red",width=1,text="1", font= "arial")
+btn.pack()
+btn=Button ()
+btn.place(x=102, y=18)
 
 
-def printsum():
-    uFirst=u_first_num.get()
-    uSecond=u_second_num.get()
-    num2=int(uSecond)
-    num1=int(uFirst)
-    result=num1+num2
-    resultVar.set(result)
-btn=Button(window,bg="red",width=1,text="+", font= "arial",command=printsum)
+btn=Button(window,bg="red",width=1,text="2", font= "arial")
 btn.pack()
-def printsub():
-    uFirst=u_first_num.get()
-    uSecond=u_second_num.get()
-    num2=int(uSecond)
-    num1=int(uFirst)
-    result=num1-num2
-    resultVar.set(result)
-btn=Button(window,bg="red",width=1,text="-", font= "arial",command=printsub)
+btn=Button ()
+btn.place(x=25, y=100)
+
+
+btn=Button(window,bg="red",width=1,text="3", font= "arial")    
 btn.pack()
-def printmul():
-    uFirst=u_first_num.get()
-    uSecond=u_second_num.get()
-    num2=int(uSecond)
-    num1=int(uFirst)
-    result=num1*num2
-    resultVar.set(result)
-btn=Button(window,bg="red",width=1,text="*", font= "arial",command=printmul)    
-btn.pack()
-def printdiv():
-    uFirst=u_first_num.get()
-    uSecond=u_second_num.get()
-    num2=int(uSecond)
-    num1=int(uFirst)
-    result=num1/num2
-    resultVar.set(result)
-btn=Button(window,bg="red",width=1,text="/", font= "arial",command=printdiv)    
+btn=Button ()
+btn.place(x=25, y=100)
+
+
+
+btn=Button(window,bg="red",width=1,text="4", font= "arial")    
 btn.pack()   
+btn=Button ()
+btn.place(x=25, y=100)
+
+
+
+btn=Button(window,bg="red",width=1,text="5", font= "arial")    
+btn.pack()   
+btn=Button ()
+btn.place(x=25, y=100)
+
+btn=Button(window,bg="red",width=1,text="5", font= "arial")    
+btn.pack()   
+btn=Button ()
+btn.place(x=25, y=100)
+
+btn=Button(window,bg="red",width=1,text="6", font= "arial")    
+btn.pack()   
+btn=Button ()
+btn.place(x=25, y=100)
+
+btn=Button(window,bg="red",width=1,text="7", font= "arial")    
+btn.pack()   
+btn=Button ()
+btn.place(x=25, y=100)
+
+btn=Button(window,bg="red",width=1,text="8", font= "arial")    
+btn.pack()   
+btn=Button ()
+btn.place(x=25, y=100)
+
+btn=Button(window,bg="red",width=1,text="9", font= "arial")    
+btn.pack()   
+btn=Button ()
+btn.place(x=25, y=100)
+
+
+btn=Button(window,bg="red",width=1,text="0", font= "arial")    
+btn.pack()   
+btn=Button ()
+btn.place(x=25, y=100)
+
+btn=Button(window,bg="red",width=1,text="=", font= "arial")
+btn.pack()
+btn=Button ()
+btn.place(x=102, y=30)
+
+btn=Button(window,bg="red",width=1,text="+", font= "arial")
+btn.pack()
+btn=Button ()
+btn.place(x=102, y=60)
+
+btn=Button(window,bg="red",width=1,text="-", font= "arial")
+btn.pack()
+btn=Button ()
+btn.place(x=102, y=90)
+
+btn=Button(window,bg="red",width=1,text=".", font= "arial")
+btn.pack()
+btn=Button ()
+btn.place(x=102, y=120)
+
+btn=Button(window,bg="red",width=1,text="/", font= "arial")
+btn.pack()
+btn=Button ()
+btn.place(x=102, y=150)
+
+btn=Button(window,bg="red",width=1,text="AC", font= "arial")
+btn.pack()
+btn=Button ()
+btn.place(x=102, y=180)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 labeleage=Label(window,bg="grey",fg="yellow",font="arial",width="30", textvariable= resultVar)
 labeleage.pack()
-window.mainloop()
+window.mainloop() 

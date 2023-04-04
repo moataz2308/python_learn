edab2455e272ec40bdc1bb859f4bf361c7468c31
Moataz <moataz.odense@gmail.com>
2023-04-04 14:09:04 +0200

    root

--- calculator.py
+++ calculator.py
@@ -8,158 +8,98 @@ resultVar=StringVar()
 u_first_num=Entry(window,bg="blue",fg="white",font="arial",width=15,text='1 ',borderwidth=2, relief="groove")
 u_first_num.pack() 
 
-btn=Button(window,bg="red",width=1,text="1", font= "arial")
-btn.pack()
-btn=Button ()
-btn.place(x=102, y=18)
 
 
-btn=Button(window,bg="red",width=1,text="2", font= "arial")
-btn.pack()
-btn=Button ()
-btn.place(x=25, y=100)
+btn2=Button(window,bg="red",width=1,text="1", font= "arial")
+btn2.pack()
+btn2.place(x=25, y=-30)
+
+
+btn3=Button(window,bg="red",width=1,text="2", font= "arial")    
+btn3.pack()
+btn3.place(x=25, y=-60)
+
 
 
 btn=Button(window,bg="red",width=1,text="3", font= "arial")    
-btn.pack()
-btn=Button ()
-btn.place(x=25, y=100)
+btn.pack()   
+btn.place(x=25, y=90)
 
 
 
 btn=Button(window,bg="red",width=1,text="4", font= "arial")    
 btn.pack()   
-btn=Button ()
-btn.place(x=25, y=100)
-
-
+btn.place(x=25, y=120)
 
 btn=Button(window,bg="red",width=1,text="5", font= "arial")    
 btn.pack()   
-btn=Button ()
-btn.place(x=25, y=100)
-
-btn=Button(window,bg="red",width=1,text="5", font= "arial")    
-btn.pack()   
-btn=Button ()
-btn.place(x=25, y=100)
+btn.place(x=25, y=150)
 
 btn=Button(window,bg="red",width=1,text="6", font= "arial")    
 btn.pack()   
-btn=Button ()
-btn.place(x=25, y=100)
+
+btn.place(x=25, y=180)
 
 btn=Button(window,bg="red",width=1,text="7", font= "arial")    
 btn.pack()   
-btn=Button ()
-btn.place(x=25, y=100)
+
+btn.place(x=25, y=210)
 
 btn=Button(window,bg="red",width=1,text="8", font= "arial")    
 btn.pack()   
-btn=Button ()
-btn.place(x=25, y=100)
+
+btn.place(x=25, y=240)
 
 btn=Button(window,bg="red",width=1,text="9", font= "arial")    
 btn.pack()   
-btn=Button ()
-btn.place(x=25, y=100)
+
+btn.place(x=25, y=270)
 
 
 btn=Button(window,bg="red",width=1,text="0", font= "arial")    
 btn.pack()   
-btn=Button ()
-btn.place(x=25, y=100)
+
+btn.place(x=25, y=300)
+
+btn=Button(window,bg="red",width=1,text="1", font= "arial")    
+btn.pack()   
+
+btn.place(x=25, y=330)
+
+btn=Button(window,bg="red",width=1,text="2", font= "arial")    
+btn.pack()   
+
+btn.place(x=25, y=360)
 
 btn=Button(window,bg="red",width=1,text="=", font= "arial")
 btn.pack()
-btn=Button ()
-btn.place(x=102, y=30)
+
+btn.place(x=102, y=220)
 
 btn=Button(window,bg="red",width=1,text="+", font= "arial")
 btn.pack()
-btn=Button ()
-btn.place(x=102, y=60)
+
+btn.place(x=102, y=250)
 
 btn=Button(window,bg="red",width=1,text="-", font= "arial")
 btn.pack()
-btn=Button ()
-btn.place(x=102, y=90)
+
+btn.place(x=102, y=280)
 
 btn=Button(window,bg="red",width=1,text=".", font= "arial")
 btn.pack()
-btn=Button ()
-btn.place(x=102, y=120)
+
+btn.place(x=102, y=310)
 
 btn=Button(window,bg="red",width=1,text="/", font= "arial")
 btn.pack()
-btn=Button ()
-btn.place(x=102, y=150)
+
+btn.place(x=102, y=330)
 
 btn=Button(window,bg="red",width=1,text="AC", font= "arial")
 btn.pack()
-btn=Button ()
-btn.place(x=102, y=180)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btn.place(x=102, y=360)
 
 
 labeleage=Label(window,bg="grey",fg="yellow",font="arial",width="30", textvariable= resultVar)
---
+++ seshion1.py
@@ -0,0 +1,15 @@
+from tkinter import*
+root=Tk()
+myLabel1=Label(root,text=("Hello maltys:)"))
+myLabel2=Label(root,text=("my name is Moataz:)"))
+myLabel3=Label(root,text=("                 "))
+
+
+myLabel1.grid(row=0,column=0)
+myLabel2.grid(row=1,column=5)
+myLabel3.grid(row=1,column=1)
+
+
+
+
+root.mainloop()

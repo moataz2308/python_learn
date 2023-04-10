341c420c4b76d8c581bfb94a184a098df6723944
Moataz <moataz.odense@gmail.com>
2023-04-11 00:11:38 +0200

    working on open image

--- session8.py
+++ session8.py
@@ -1,9 +1,30 @@
 from tkinter import*
+from PIL import ImageTk,Image
 root=Tk()
 root.title("learn with codemy.com")
 root.iconbitmap('/Users/passant/Moataz/game-boy.ico')
+my_img=ImageTk.PhotoImage(Image.open())
+my_label=Label(image=my_img)
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 button_quit=Button(root,text="exit programm",command=root.quit)
 button_quit.pack()
+
 
 
 

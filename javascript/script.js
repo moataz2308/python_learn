51018e0ec2ae94b72d60e9af4546dfa63a01b966
Moataz <moataz.odense@gmail.com>
2023-07-08 20:04:22 +0200

    latest

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,10 @@
-console.log("hello Javascript")
+var number;
 
-    
-   
+for(number= 1; number <= 10; number=number +1){
+if(number%2 == 0){
+   console.log("Gerade zahl");
+}
+else{
+   console.log("Un gerader zahl");
+}
+}
